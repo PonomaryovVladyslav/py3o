97d1c678db45f0aeacf7422a8a2249ba62e6dff4
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2023-01-18 21:12:02 +0100

    first homework

--- homeworks/urls.py
+++ homeworks/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import test
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('blabla/', test)
 ]
